4ebc45a7cf989afb2fc9c7089addb334180cd71e
downloadfromhereail.com <markusdorfer1@gmail.com>
2018-08-18 15:57:39 +0200

    first commit

---
+++ config.js
@@ -0,0 +1,18 @@
+// host = '172.16.153.122';	// hostname or IP address
+host = '10.0.0.25';	// hostname or IP address
+// host = 'iot.eclipse.org';	// hostname or IP address
+// port = 9001;
+port = 9001;
+topic = 'LED';		// topic to publish to
+moisture_topic = 'plant_1/status/moisture';		// Moisture
+useTLS = false;
+username = null;
+password = null;
+// path as in "scheme:[//[user:password@]host[:port]][/]path[?query][#fragment]"
+//    defaults to "/mqtt"
+//    may include query and fragment
+//
+// path = "/mqtt";
+// path = "/data/cloud?device=12345";
+
+cleansession = true;
---
+++ script.js
@@ -0,0 +1,86 @@
+
+var mqtt;
+	var reconnectTimeout = 2000;
+	var Timestamp;
+
+	function MQTTconnect() {
+		if (typeof path == "undefined") {
+			path = '/mqtt';
+		}
+			
+		mqtt = new Paho.MQTT.Client(
+				host,
+				port,
+				path,
+				"web_" + parseInt(Math.random() * 100, 10)
+				);
+			
+		var options = {
+			timeout: 3,
+			useSSL: useTLS,
+			cleanSession: cleansession,
+			onSuccess: onConnect,
+			onFailure: function (message) {
+				$('#status').val("Connection failed: " + message.errorMessage + "Retrying");
+				setTimeout(MQTTconnect, reconnectTimeout);
+			}
+		};
+
+		mqtt.onConnectionLost = onConnectionLost;
+		mqtt.onMessageArrived = onMessageArrived;
+
+		if (username != null) {
+			options.userName = username;
+			options.password = password;
+		}
+		//console.log("Host="+ host + ", port=" + port + ", path=" + path + " TLS = " + useTLS + " username=" + username + " password=" + password);
+		mqtt.connect(options);
+	}
+
+	//Called when successfully connected to MQTT_Broker
+	function onConnect() {	
+		mqtt.subscribe(moisture_topic, {qos: 0});
+		$('#status').val('Connected to ' + host + ':' + port + path);
+	}
+
+	//Called if web client lost tconnection to the MQTT_Broker
+	function onConnectionLost(responseObject) {
+		setTimeout(MQTTconnect, reconnectTimeout);
+		$('#status').val("connection lost: " + responseObject.errorMessage + ". Reconnecting");
+	};
+
+	//Called if any message arrives
+	function onMessageArrived(message) {
+		var topic = message.destinationName;
+		var payload = message.payloadString;
+		var moisture = parseInt(payload)+3;
+				
+		if(topic == moisture_topic) {
+			//$('#ws').prepend('<li>' + moisture + '</li>');
+			//document.getElementById("myRange").value = moisture;
+		}
+		//Print additional information
+		//$('#ws').prepend('<li>' + topic + ' = ' + payload + '</li>');
+	};
+			
+	
+	function SendRGB(red, green, blue) {
+		mqtt.send("LED_Bett/commands/colorpicker", "rgb("+red+","+green+","+blue+")", 1, false);
+	}
+			
+	function SendCommand(action) {
+		topic="LED_Bett/commands/"+action;
+		mqtt.send(topic, "1", 1, false);
+	}
+			
+
+  			
+  			
+  			$(document).ready(function() {
+  				MQTTconnect();
+  			});
+
+
+	
+	
+			
